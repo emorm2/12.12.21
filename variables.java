fbdf1bc0144abc1092d530ae0d5a0ef3877f55f2
emorm2 <96049316+emorm2@users.noreply.github.com>
2021-12-13 11:21:06 +0200

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+  // misparim shlemim:
+       int mispar;
+       mispar = 3;
+       int number = 4;
+       byte num = 31;
+       // short 3num;  error!
+        short $num;
+        long num3 = 10;
+
+        // point number:
+        double d = 5.2;
+        float f = 3.4f;
+
+        char c = '#';
+        boolean b = true;
+
+        String s = "Einat";
+        System.out.println(mispar);
+
+    // program that takes 2 variabl
+    public static void main(String[] args) {
+        String stringNew = "Einat";
+        String stringNew1 = "roy";
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number = s.nextInt();
+        System.out.println(number);
+    }
+}
